ecd44c4a40a4445e36c17f31db58d8108063550f
bruno130187 <bruno130187@gmail.com>
2023-03-21 09:09:22 -0300

    enviando projeto final com docker, zookeper e 2 servidores kafka

--- src/main/java/br/com/bruno/http/EcommerceServiceHttp.java
+++ src/main/java/br/com/bruno/http/EcommerceServiceHttp.java
@@ -5,7 +5,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-public class ecommerceServiceHttp {
+public class EcommerceServiceHttp {
 
     public static void main(String[] args) throws Exception {
 
